da3e83562e2a593f5a5ae3085f97a24aef3eaa94
koyuta <koyuuta5330@gmail.com>
2017-05-04 13:47:17 +0900

    Fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+VIM_DIR=$HOME/.vimmmm
+VIM_RC_DIR=$VIM_DIR/rc
+VIM_RCD_DIR=$VIM_DIR/vimrc.d
+
+for dir in $VIM_RC_DIR $VIM_RCD_DIR; do
+    if [ ! -e $dir ]; then
+        mkdir -p $dir
+    fi
+done
+
 DOT_FILES=(.gitconfig .gitconfig_global .tmux.conf .vimrc .zshrc)
 
 VIM_RC_FILES=(dein.toml dein_lazy.toml )
