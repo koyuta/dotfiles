f036e73a92df0be34ac0eab953ce711922c7417a
misawa_koyuta <misawa_koyuta@cyberagent.co.jp>
2019-01-11 19:26:39 +0900

    :up:

--- setup.sh
+++ setup.sh
@@ -3,38 +3,31 @@ set -e
 
 DOTFILE_DIR=$HOME/dotfiles
 
-VIM_DIR=.vim
+XDG_CONFIG_HOME=$HOME/.config
+
+NEOVIM_DIR=$XDG_CONFIG_HOME/nvim
+ALACRITTY_DIR=$XDG_CONFIG_HOME/alacritty
+
+VIM_DIR=$HOME/.vim
 VIM_RC_DIR=$VIM_DIR/rc
 VIM_RCD_DIR=$VIM_DIR/vimrc.d
 
-for dir in $HOME/$VIM_RC_DIR $HOME/$VIM_RCD_DIR; do
-    if [ ! -e $dir ]; then
-        mkdir -p $dir
-    fi
-done
+# conf files that set on $XDG_CONFIG_HOME/
+[ -e $XDG_CONFIG_HOME ] || mkdir -p $XDG_CONFIG_HOME
 
-NEOVIM_DIR=$HOME/.config/nvim
+[ -e $ALACRITTY_DIR ] || mkdir -p $ALACRITTY_DIR
+ln -nfs $DOTFILE_DIR/alacritty.yml $ALACRITTY_DIR/alacritty.yml
 [ -e $NEOVIM_DIR ] || mkdir -p $NEOVIM_DIR
 ln -nfs $DOTFILE_DIR/.vimrc $NEOVIM_DIR/init.vim
 
+# conf files that set on $HOME/
 DOT_FILES=(.gitcommit_template .gitconfig .gitignore_global .tmux.conf .vimrc .zshrc .hyper.js)
-
-VIM_RC_FILES=$(ls $DOTFILE_DIR/$VIM_RC_DIR)
-VIM_RCD_FILES=$(ls $DOTFILE_DIR/$VIM_RCD_DIR)
-
 for file in ${DOT_FILES[@]};do
-    ln -nfs $HOME/dotfiles/$file $HOME/$file
+    ln -nfs $DOTFILE_DIR/$file $HOME/$file
 done
 
-for file in ${VIM_RC_FILES[@]};do
-    ln -nfs $HOME/dotfiles/.vim/rc/$file $HOME/.vim/rc/$file
+for dir in $VIM_RC_DIR $VIM_RCD_DIR; do
+    [ ! -e $dir ] || mkdir -p $dir
 done
-
-for file in ${VIM_RCD_FILES[@]};do
-    ln -nfs $HOME/dotfiles/.vim/vimrc.d $HOME/.vim/vimrc.d
-done
-
-[ -z $XDG_CONFIG_HOME ] && XDG_CONFIG_HOME=$HOME/.config
-
-[ -e $XDG_CONFIG_HOME ] || mkdir -p $XDG_CONFIG_HOME
-ln -nfs $DOTFILE_DIR/alacritty.yml $XDG_CONFIG_HOME/alacritty/alacritty.yml
+ln -nfs $DOTFILE_DIR/.vim/rc $VIM_RC_DIR
+ln -nfs $DOTFILE_DIR/.vim/vimrc.d $VIM_RCD_DIR
