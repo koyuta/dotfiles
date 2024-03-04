91f9d374723f41b481226b807162f178332f1f75
misawa_koyuta <misawa_koyuta@cyberagent.co.jp>
2024-03-04 19:34:01 +0900

    :up:

--- .zshrc
+++ .zshrc
@@ -1,3 +1,18 @@
+ulimit -n 4096
+
+# prompt setting
+autoload -Uz colors && colors
+PROMPT="%{${fg[cyan]}%}[%~]%{${reset_color}%}%1(v| %F{red}%1v%f|) "
+
+# path setting
+export PATH=/usr/local/bin:/usr/local/sbin:/bin:/sbin:/opt/bin:/usr/bin:$PATH
+
+# homebrew for apple silicon
+if [[ `uname -p` == "arm" ]]; then
+  HOMEBREW_ROOT=/opt/homebrew
+  export PATH=$HOMEBREW_ROOT/bin:$PATH
+fi
+
 # attach tmux when loggin
 if [[ -z "$TMUX" ]]; then
   tmux attach || tmux new-session
--- zsh/tools.zsh
+++ zsh/tools.zsh
@@ -1,9 +1,3 @@
-# homebrew for apple silicon
-if [[ `uname -p` == "arm" ]]; then
-  HOMEBREW_ROOT=/opt/homebrew
-  export PATH=$HOMEBREW_ROOT/bin:$PATH
-fi
-
 # keychain
 if [ ! -f $HOME/.keychain/$HOSTNAME-sh ] ; then
   unset SSH_AUTH_SOCK
@@ -18,30 +12,13 @@ export PATH="$PATH:$OPENSSL_ROOT/bin"
 export VSCODE_ROOT="/Applications/Visual\ Studio\ Code.app/Contents/Resources/app"
 export PATH="$PATH:$VSCODE_ROOT/bin"
 
-# pyenv
-#export PYENV_ROOT="$HOME/.pyenv"
-#export PATH="$PYENV_ROOT/bin:$PATH"
-#eval "$(pyenv init -)"
-
-# rbenv
-#export RBENV_ROOT="$HOME/.rbenv"
-#export PATH="$RBENV_ROOT/bin:$PATH"
-#eval "$(rbenv init -)"
-
-# nodebrew
-#export NODEBREW_ROOT="$HOME/.nodebrew"
-#export PATH="$NODEBREW_ROOT/current/bin:$PATH"
+# mise
+eval "$(~/.local/bin/mise activate zsh)"
 
 # go
 export PATH=$PATH:/usr/local/go/bin
 export GOPATH="$HOME/go"
 export PATH="$PATH:$GOPATH/bin"
-
-# goenv
-export GOENV_ROOT="$HOME/.goenv"
-export PATH="$GOENV_ROOT/bin:$PATH"
-export GOENV_DISABLE_GOPATH=1
-eval "$(goenv init -)"
 
 # rust
 export CARGO_PATH="$HOME/.cargo"
